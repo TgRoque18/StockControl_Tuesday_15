295f3a9f67bb0287f6f4e41baff1600853aa8f70
AlunoLU <AlunoLU@LEVELUP09-PC>
2017-12-12 12:15:57 -0200

    Começando a criação do Form Home e Arrumando o nome dos itens do form "Login" para que ficasse em inglês

---
+++ StockControl/StockControl/Forms/Home.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace StockControl.Forms
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxBack
+            // 
+            this.pbxBack.Image = global::StockControl.Properties.Resources.voltar;
+            this.pbxBack.Location = new System.Drawing.Point(3, 228);
+            this.pbxBack.Name = "pbxBack";
+            this.pbxBack.Size = new System.Drawing.Size(33, 32);
+            this.pbxBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxBack.TabIndex = 0;
+            this.pbxBack.TabStop = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pbxBack);
+            this.Name = "Home";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxBack;
+    }
+}
--- StockControl/StockControl/Properties/Resources.Designer.cs
+++ StockControl/StockControl/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace StockControl.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap voltar {
+            get {
+                object obj = ResourceManager.GetObject("voltar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
