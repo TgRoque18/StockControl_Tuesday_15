7b8821baf6657d17ef5de15f082ac9771bf3929a
AlunoLU <AlunoLU@LEVELUP06-PC>
2017-12-12 16:22:59 -0200

    Criado Form Log Details Criado Form User Profile All

---
+++ StockControl/StockControl/Forms/LogDetailsForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockControl.Forms
+{
+    public partial class LogDetailsForm : Form
+    {
+        public LogDetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pbxSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbxBack_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
