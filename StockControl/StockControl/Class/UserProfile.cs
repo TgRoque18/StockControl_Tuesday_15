73a40519a6e5c217438bcd52feed4bf69454212b
Miguelalt <miguelalt1@gmail.com>
2017-12-13 19:59:28 -0200

    Criada classe UserProfile (falta identar e criar construtor)

---
+++ StockControl/StockControl/Class/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockControl.Class
+{
+   public class UserProfile
+    {
+
+        private int id;
+        private bool active;
+        private char name;
+
+    }
+}
