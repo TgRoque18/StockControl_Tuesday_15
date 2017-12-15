e93f47efc6127f450ffbfec39e7c632f503549fa
Miguelalt <miguelalt1@gmail.com>
2017-12-15 01:32:36 -0200

    Edição da classe UserProfile

--- StockControl/StockControl/Class/UserProfile.cs
+++ StockControl/StockControl/Class/UserProfile.cs
@@ -6,12 +6,36 @@ using System.Threading.Tasks;
 
 namespace StockControl.Class
 {
-   public class UserProfile
+    public class UserProfile
     {
-
-        private int id;
+        private int id = 0;
         private bool active;
         private char name;
 
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public bool Active
+        {
+            get => active;
+            set => active = value;
+        }
+
+        public char Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+
+        public UserProfile(int id, bool active, char name)
+        {
+            this.Id = id;
+            this.Active = active;
+            this.Name = name;
+        }
     }
 }
