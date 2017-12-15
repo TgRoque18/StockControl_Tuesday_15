e6851578f9642e8e732637785d99319a9ef802f1
Miguelalt <miguelalt1@gmail.com>
2017-12-15 01:40:20 -0200

    Criada a classe Log_Type (Preenchida)

---
+++ StockControl/StockControl/Class/Log_Type.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockControl.Class
+{
+    public class Log_Type
+    {
+        private int id;
+        private char name;
+        private byte active;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public char Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public byte Active
+        {
+            get => active;
+            set => active = value;
+        }
+
+        public Log_Type(int id, char name, byte active)
+        {
+            this.Active = active;
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
