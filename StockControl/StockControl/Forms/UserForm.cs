dddd8c58392fbb1b54eb5bc52d24925229e22d75
Joao P Reys <jpreysm@gmail.com>
2017-12-19 16:08:37 -0200

    criado o form UserAll, construtor da classe email e o openfiledialog do form user

--- StockControl/StockControl/Forms/UserForm.cs
+++ StockControl/StockControl/Forms/UserForm.cs
@@ -12,14 +12,33 @@ namespace StockControl.Forms
 {
     public partial class UserForm : Form
     {
+        Image profilePicture;
+        string fileName;
+
         public UserForm()
         {
             InitializeComponent();
             
         }
 
+        private void pbxAddImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fileName = ofdSearchProfilePicture.FileName;
+                tbxFileName.Text = fileName;
+                profilePicture = Image.FromFile(fileName);
+                pbxAddProfilePicture.Image = profilePicture;
 
+                
+              
+            }
+            
+            catch (Exception ex)
+            {
 
-  
+                MessageBox.Show("Não é possível exibir a imagem \n\nErro reportado : " + ex.Message);
+            }
+        }
     }
 }
