f6f6d000f665dcc96799b45523838b13600e84fc
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-30 16:17:20 -0200

    Ligando os forms Log e UserPRofile ao banco

--- StockControl/StockControl/Forms/LogAllForm.cs
+++ StockControl/StockControl/Forms/LogAllForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace StockControl.Forms
 {
     public partial class LogAllForm : Form
     {
+        
         public LogAllForm()
         {
             InitializeComponent();
