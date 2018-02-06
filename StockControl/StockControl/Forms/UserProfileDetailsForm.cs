ee933a88b4afe04642b8bc5c4c44a52a1786cc75
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-02-06 17:13:33 -0200

    Criado Update e Insert

--- StockControl/StockControl/Forms/UserProfileDetailsForm.cs
+++ StockControl/StockControl/Forms/UserProfileDetailsForm.cs
@@ -24,6 +24,59 @@ namespace StockControl
             InitializeComponent();
         }
 
+        public UserProfileForm(int idUserProfile)
+        {
+            InitializeComponent();
+
+            lblId.Text = idUserProfile.ToString(); 
+
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+            if (!string.IsNullOrEmpty(lblId.Text))
+            {
+                try
+                {
+                    //Conectar
+                    sqlConnect.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM USER_PROFILE WHERE ID = @id", sqlConnect);
+                    //SqlCommand cmd = new SqlCommand("SELECT * FROM CATEGORY WHERE ID = " + idCategory.ToString(), sqlConnect);
+
+                    cmd.Parameters.Add(new SqlParameter("@id", idUserProfile));
+
+                    UserProfile userProfile = new UserProfile(); //------
+
+                    using (SqlDataReader reader = cmd.ExecuteReader()) //-----
+                    {
+                        while (reader.Read())
+                        {
+                            userProfile.Id = Int32.Parse(reader["ID"].ToString());
+                            userProfile.Name = reader["NAME"].ToString();
+                            userProfile.Active = bool.Parse(reader["ACTIVE"].ToString());
+                        }
+                    }
+
+                    tbxName.Text = userProfile.Name;
+                    cbxActive.Checked = userProfile.Active;
+                    
+
+                    
+
+                }
+                catch (Exception EX)
+                {
+                    //Tratar exce��es
+                    throw;
+                }
+                finally
+                {
+                    //Fechar
+                    sqlConnect.Close();
+                }
+            }
+        }
+
+
         private void pbxBack_Click(object sender, EventArgs e)
         {
             UserProfileAllForm userProfile = new UserProfileAllForm();
@@ -33,43 +86,80 @@ namespace StockControl
 
         private void pbxSave_Click(object sender, EventArgs e)
         {
-            GetData();
-            SqlConnection sqlConnect = new SqlConnection(connectionString);
-
-            try
+            if (string.IsNullOrEmpty(lblId.Text))
             {
-                //Conectar
-                sqlConnect.Open();
-                string sql = "INSERT INTO USER_PROFILE(NAME, ACTIVE) VALUES (@name, @active)";
-                //string sql = "INSERT INTO CATEGORY(NAME, ACTIVE) VALUES (" 
-                //    + this.tbxName.Text + "," + this.cbxActive.Checked + ")";
+                GetData();
+                SqlConnection sqlConnect = new SqlConnection(connectionString);
 
-                SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+                try
+                {
+                    //Conectar
+                    sqlConnect.Open();
+                    string sql = "INSERT INTO USER_PROFILE(NAME, ACTIVE) VALUES (@name, @active)";
+                    //string sql = "INSERT INTO CATEGORY(NAME, ACTIVE) VALUES (" 
+                    //    + this.tbxName.Text + "," + this.cbxActive.Checked + ")";
 
-                cmd.Parameters.Add(new SqlParameter("@name", name));
-                cmd.Parameters.Add(new SqlParameter("@active", active));
+                    SqlCommand cmd = new SqlCommand(sql, sqlConnect);
 
-                cmd.ExecuteNonQuery();
+                    cmd.Parameters.Add(new SqlParameter("@name", name));
+                    cmd.Parameters.Add(new SqlParameter("@active", active));
 
-                MessageBox.Show("Adicionado com sucesso!");
-                Log.SalvarLog("Adicionado perfil de usuário", DateTime.Now);
-                CleanData();
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Adicionado com sucesso!");
+                    Log.SalvarLog("Adicionado perfil de usuário", DateTime.Now);
+                    CleanData();
+                }
+
+                catch (Exception ex)
+                {
+                    //Tratar exce��es
+                    MessageBox.Show("Erro ao adicionar perfil de usuário !" + ex.Message);
+                    CleanData();
+                }
+
+                finally
+                {
+                    //Fechar
+                    sqlConnect.Close();
+
+                }
+
             }
 
-            catch (Exception ex)
+            else
             {
-                //Tratar exce��es
-                MessageBox.Show("Erro ao adicionar perfil de usuário !" + ex.Message);
-                CleanData();
+                SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+                try
+                {
+                    sqlConnect.Open();
+                    string sql = "UPDATE USER_PROFILE SET NAME = @name, ACTIVE = @active Where ID = @id";
+
+                    SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+                    cmd.Parameters.Add(new SqlParameter("@id", lblId.Text));
+                    cmd.Parameters.Add(new SqlParameter("@name", this.tbxName.Text));
+                    cmd.Parameters.Add(new SqlParameter("@active", this.cbxActive.Checked));
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Alterações salvas com sucesso!");
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show("Erro ao editar esta perfil de usuário!" + "\n\n" + Ex.Message);
+                    throw;
+                }
+                finally
+                {
+                    sqlConnect.Close();
+
+                    UserProfileAllForm userProfile = new UserProfileAllForm();
+                    userProfile.Show();
+                    this.Hide();
+                }
+
             }
-
-            finally
-            {
-                //Fechar
-                sqlConnect.Close();
-
-            }
-
 
         }
 
@@ -85,6 +175,7 @@ namespace StockControl
             name = tbxName.Text;
             active = cbxActive.Checked;
         }
+
         void CleanData()
         {
             tbxName.Text = "";
